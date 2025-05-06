79800b238ff54f821c9a8a470e9b7b1d1235594c
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-05-06 20:54:47 +0100

    03.02 - creating your first RESTful API - setup UserController that returns a list of users at the users endpoint. use the UserRepository to handle creating the User list

---
+++ src/main/java/com/jparkkennaby/store/controllers/UserController.java
@@ -0,0 +1,21 @@
+package com.jparkkennaby.store.controllers;
+
+import com.jparkkennaby.store.entities.User;
+import com.jparkkennaby.store.repositories.UserRepository;
+
+import lombok.AllArgsConstructor;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class UserController {
+    private final UserRepository userRepository;
+
+    // GetMapping just handles the GET method
+    @GetMapping("/users")
+    public Iterable<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
