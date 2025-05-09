a1492bd52d07085080b9330f3bdcf072e3e946c9
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-05-09 15:58:45 +0100

    mapstruct fix: update Mapper annotation on UserMapper. add unmappedTargetPolicy + comments (custom code not in course)

--- src/main/java/com/jparkkennaby/store/mappers/UserMapper.java
+++ src/main/java/com/jparkkennaby/store/mappers/UserMapper.java
@@ -1,11 +1,15 @@
 package com.jparkkennaby.store.mappers;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
 import com.jparkkennaby.store.dtos.UserDto;
 import com.jparkkennaby.store.entities.User;
 
-@Mapper(componentModel = "spring") // so spring is made aware and can make beans at runtime
+@Mapper(
+    componentModel = "spring", // so spring is made aware and can make beans at runtime
+    unmappedTargetPolicy = ReportingPolicy.IGNORE // ignores unmapped fields
+) 
 public interface UserMapper {
     UserDto toDto(User user);
 }
