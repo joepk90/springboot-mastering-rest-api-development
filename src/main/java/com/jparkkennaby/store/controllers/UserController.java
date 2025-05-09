963def291be2ed7d410a3994d92349daa0278ae0
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-05-09 11:32:55 +0100

    03.04 - creating dynamic routes - udpate the UserController to handle the users request mapping at the class instead of the method using the RequestMapping annotation

--- src/main/java/com/jparkkennaby/store/controllers/UserController.java
+++ src/main/java/com/jparkkennaby/store/controllers/UserController.java
@@ -7,20 +7,22 @@ import lombok.AllArgsConstructor;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/users")
 public class UserController {
     private final UserRepository userRepository;
 
     // GetMapping just handles the GET method
-    @GetMapping("/users")
+    @GetMapping
     public Iterable<User> getAllUsers() {
         return userRepository.findAll();
     }
 
-    @GetMapping("/users/{id}")
+    @GetMapping("/{id}")
     public User getUser(@PathVariable Long id) {
         return userRepository.findById(id).orElse(null);
     }
