cf6e774930520c6dc071d876875717b9883a53a1
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-05-09 11:31:32 +0100

    03.04 - creating dynamic routes - setup new users route that returns a single user using a user id

--- src/main/java/com/jparkkennaby/store/controllers/UserController.java
+++ src/main/java/com/jparkkennaby/store/controllers/UserController.java
@@ -6,6 +6,7 @@ import com.jparkkennaby.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,5 +18,10 @@ public class UserController {
     @GetMapping("/users")
     public Iterable<User> getAllUsers() {
         return userRepository.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User getUser(@PathVariable Long id) {
+        return userRepository.findById(id).orElse(null);
     }
 }
