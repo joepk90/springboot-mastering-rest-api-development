88f9b2ede742ebc163d1bc7899dce2dc93c6d17f
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-05-10 16:23:28 +0100

    03.08 - customizing response data - setup examples of customising the response data in the UserDto class and UserMapper interface

--- src/main/java/com/jparkkennaby/store/dtos/UserDto.java
+++ src/main/java/com/jparkkennaby/store/dtos/UserDto.java
@@ -1,5 +1,11 @@
 package com.jparkkennaby.store.dtos;
 
+// import java.time.LocalDateTime;
+// import com.fasterxml.jackson.annotation.JsonFormat;
+// import com.fasterxml.jackson.annotation.JsonInclude;
+// import com.fasterxml.jackson.annotation.JsonIgnore;
+// import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -12,7 +18,15 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class UserDto {
+    // @JsonIgnore // ignore the annotated field
+    // @JsonProperty("user_id") // rename the annotated field
     private Long id;
     private String name;
     private String email;
+
+    // @JsonInclude(JsonInclude.Include.NON_NULL) // exlcude null fields from response
+    // private String phoneNumber;
+
+    // @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    // private LocalDateTime createdAt; // filed populated in the UserMapper class 
 }
--- src/main/java/com/jparkkennaby/store/mappers/UserMapper.java
+++ src/main/java/com/jparkkennaby/store/mappers/UserMapper.java
@@ -1,11 +1,13 @@
 package com.jparkkennaby.store.mappers;
 
 import org.mapstruct.Mapper;
+// import org.mapstruct.Mapping;
 
 import com.jparkkennaby.store.dtos.UserDto;
 import com.jparkkennaby.store.entities.User;
 
 @Mapper(componentModel = "spring") // so spring is made aware and can make beans at runtime
 public interface UserMapper {
+    // @Mapping(target = "createdAt", expression = "java(java.time.LocalDateTime.now())")
     UserDto toDto(User user);
 }
