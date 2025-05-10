24bebba782e70606543bccd5d8a933a8d3a8db24
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-05-10 18:48:05 +0100

    03.12 - extracting request body - setup new createUser method on the UserController to accept Post requests and return the request body

--- src/main/java/com/jparkkennaby/store/controllers/UserController.java
+++ src/main/java/com/jparkkennaby/store/controllers/UserController.java
@@ -13,6 +13,8 @@ import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 // import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -56,5 +58,10 @@ public class UserController {
         
          // return new ResponseEntity<>(user, HttpStatus.OK);
          return ResponseEntity.ok(userMapper.toDto(user));
+    }
+
+    @PostMapping
+    public UserDto createUser(@RequestBody UserDto data) {
+        return data;
     }
 }
