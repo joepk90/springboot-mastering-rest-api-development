66dd6cd946e42a2066f4b40b895e919ad95c5a35
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-05-10 16:35:32 +0100

    03.09 - extracing query paramters - setup a sort RequestParam on the UserController getAllUsers method - pass sort condition to the UserRepository findAll function

--- src/main/java/com/jparkkennaby/store/controllers/UserController.java
+++ src/main/java/com/jparkkennaby/store/controllers/UserController.java
@@ -6,11 +6,13 @@ import com.jparkkennaby.store.repositories.UserRepository;
 
 import lombok.AllArgsConstructor;
 
+import org.springframework.data.domain.Sort;
 // import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,8 +24,8 @@ public class UserController {
 
     // GetMapping just handles the GET method
     @GetMapping
-    public Iterable<UserDto> getAllUsers() {
-        return userRepository.findAll()
+    public Iterable<UserDto> getAllUsers(@RequestParam String sort) {
+        return userRepository.findAll(Sort.by(sort))
             .stream()
             .map(userMapper::toDto)
             .toList();
