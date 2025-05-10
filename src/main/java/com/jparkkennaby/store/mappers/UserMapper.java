bb29a24f8f71c05d69d08f7ce45fe9afe714f6fd
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-05-10 16:26:53 +0100

    Revert "mapstruct fix: update Mapper annotation on UserMapper. add unmappedTargetPolicy + comments (custom code not in course)"
    This reverts commit a1492bd52d07085080b9330f3bdcf072e3e946c9.

--- src/main/java/com/jparkkennaby/store/mappers/UserMapper.java
+++ src/main/java/com/jparkkennaby/store/mappers/UserMapper.java
@@ -1,15 +1,11 @@
 package com.jparkkennaby.store.mappers;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.ReportingPolicy;
 
 import com.jparkkennaby.store.dtos.UserDto;
 import com.jparkkennaby.store.entities.User;
 
-@Mapper(
-    componentModel = "spring", // so spring is made aware and can make beans at runtime
-    unmappedTargetPolicy = ReportingPolicy.IGNORE // ignores unmapped fields
-) 
+@Mapper(componentModel = "spring") // so spring is made aware and can make beans at runtime
 public interface UserMapper {
     UserDto toDto(User user);
 }
