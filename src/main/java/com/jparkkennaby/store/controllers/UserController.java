d1c39b4fd7fa9568d8140c7869daa8b116a89b58
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-05-10 20:33:59 +0100

    03.15 - deleting resources - setup deleteUser method in the UserController

--- src/main/java/com/jparkkennaby/store/controllers/UserController.java
+++ src/main/java/com/jparkkennaby/store/controllers/UserController.java
@@ -13,6 +13,7 @@ import java.util.Set;
 import org.springframework.data.domain.Sort;
 // import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -98,5 +99,18 @@ public class UserController {
         userRepository.save(user);
 
         return ResponseEntity.ok(userMapper.toDto(user));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        var user = userRepository.findById(id).orElse(null);
+
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        userRepository.delete(user);
+
+        return ResponseEntity.noContent().build();
     }
 }
