e1e88180550ee2284e23cae85ad642641a5230e2
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-05-11 18:31:16 +0100

    04.05 - implementing custom validation - setup boilerplate Lowercase validation interface

---
+++ src/main/java/com/jparkkennaby/store/validation/Lowercase.java
@@ -0,0 +1,17 @@
+package com.jparkkennaby.store.validation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import jakarta.validation.Payload;
+
+// @Target({ElementType.FIELD, ElementType.CONSTRUCTOR}) // handling multiple types
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Lowercase {
+    String message() default "must be lowercase";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
