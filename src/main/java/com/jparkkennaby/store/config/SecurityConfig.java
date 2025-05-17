861440ef22b0a9dfbe613bb075a25b6f30516138
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-05-17 19:47:08 +0100

    06.04 - configuring security settings - setup the SecurityFilterChain - disable stateless sessions, disable CSRF, and make all endpoints protected except for the carts endpoint

--- src/main/java/com/jparkkennaby/store/config/SecurityConfig.java
+++ src/main/java/com/jparkkennaby/store/config/SecurityConfig.java
@@ -4,16 +4,29 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
-        // stateless sessions (token-based authentication)
-        // disable CSRF (cross site request forgery)
-        // Authorize specific requests
-        return null;
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                // stateless sessions (token-based authentication)
+                .sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                // disable CSRF (cross site request forgery)
+                .csrf(AbstractHttpConfigurer::disable)
+
+                // Authorize specific requests
+                .authorizeHttpRequests(c -> c
+                        .requestMatchers("carts/**").permitAll()
+                        .anyRequest().authenticated());
+
+        // make all requests public
+        // http.authorizeHttpRequests(c -> c.anyRequest().permitAll());
+        return http.build();
     }
 }
