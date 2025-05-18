2cdcf41c035b4a305dee92e07e104b45f7378565
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-05-18 19:49:34 +0100

    06.07 - working with AuthenticationManager - setup new UserService that implements the UserDetailsService from spring security - setup loadUserByUsername method override to getting a User. setup an AuthenticationProvider in the SecutiyConfig class using the DaoAuthenticationProvider from spring securitky as well as a authenticationManager method that returns an AuthenticationManager (boilerplate)

--- src/main/java/com/jparkkennaby/store/config/SecurityConfig.java
+++ src/main/java/com/jparkkennaby/store/config/SecurityConfig.java
@@ -3,22 +3,45 @@ package com.jparkkennaby.store.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import lombok.AllArgsConstructor;
+
 @Configuration
 @EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfig {
+    private final UserDetailsService userDetailsService;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         // bcrypt is the most secure and recommended hashing algorythm
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        // The Dao Authentication Provider is the most common implementation
+        var provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(userDetailsService);
+        return provider;
     }
 
     @Bean
