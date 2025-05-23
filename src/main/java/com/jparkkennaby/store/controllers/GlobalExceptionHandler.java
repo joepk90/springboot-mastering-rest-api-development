b89630a68c4d4b1f45251e8681b1b14c741a5a21
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-05-23 16:29:53 +0100

    07.06 - improving error handling - setup new exception handler in the GlobaExceptionHandler class to return a 400 bad request response if the request body is invalid

--- src/main/java/com/jparkkennaby/store/controllers/GlobalExceptionHandler.java
+++ src/main/java/com/jparkkennaby/store/controllers/GlobalExceptionHandler.java
@@ -4,12 +4,20 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    // handles invalid request body (was returning 401 Unauthorized)
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Map<String, String>> handleUnreadableMessage() {
+        return ResponseEntity.badRequest().body(
+                Map.of("error", "Invalid request body"));
+    }
 
     // overrides the default exception handler
     @ExceptionHandler(MethodArgumentNotValidException.class)
