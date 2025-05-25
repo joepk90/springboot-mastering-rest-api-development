bf0066616473758103cfbdaf994ee6b2dc9fbd60
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-05-25 13:39:41 +0100

    08.04 - creating a checkout session - build a session object using the order data in the CheckoutService. update the checkout method to return a session url in the response - update the CheckoutResponse dto to include this property (note this breaks the CheckoutController due to the checkout method potentially returning a StripeException exception)

--- src/main/java/com/jparkkennaby/store/services/CheckoutService.java
+++ src/main/java/com/jparkkennaby/store/services/CheckoutService.java
@@ -10,6 +10,8 @@ import com.jparkkennaby.store.exceptions.CartEmptyException;
 import com.jparkkennaby.store.exceptions.CartNotFoundException;
 import com.jparkkennaby.store.repositories.CartRepository;
 import com.jparkkennaby.store.repositories.OrderRepository;
+import com.stripe.exception.StripeException;
+import com.stripe.model.checkout.Session;
 import com.stripe.param.checkout.SessionCreateParams;
 
 import lombok.RequiredArgsConstructor;
@@ -35,7 +37,7 @@ public class CheckoutService {
     @Value("${websiteUrl}")
     private String websiteUrl;
 
-    public CheckoutResponse checkout(CheckoutRequest request) {
+    public CheckoutResponse checkout(CheckoutRequest request) throws StripeException {
         var cart = cartRepository.getCartWithItems(request.getCartId()).orElse(null);
         if (cart == null) {
             throw new CartNotFoundException();
@@ -50,13 +52,30 @@ public class CheckoutService {
         orderRepository.save(order);
 
         // create checkout session
-        SessionCreateParams.builder()
+        var builder = SessionCreateParams.builder()
                 .setMode(SessionCreateParams.Mode.PAYMENT)
                 .setSuccessUrl(websiteUrl + "/checkout-success?orderId=" + order.getId())
                 .setCancelUrl(websiteUrl + "/checkout-cancel");
 
+        order.getItems().forEach(item -> {
+            var lineItem = SessionCreateParams.LineItem.builder()
+                    .setQuantity(Long.valueOf(item.getQuantity()))
+                    .setPriceData(
+                            SessionCreateParams.LineItem.PriceData.builder()
+                                    .setCurrency("usd")
+                                    .setUnitAmountDecimal(item.getUnitPrice())
+                                    .setProductData(
+                                            SessionCreateParams.LineItem.PriceData.ProductData.builder()
+                                                    .setName(item.getProduct().getName()).build())
+                                    .build())
+                    .build();
+            builder.addLineItem(lineItem);
+        });
+
+        var session = Session.create(builder.build());
+
         cartService.clearCart(cart.getId());
 
-        return new CheckoutResponse(order.getId());
+        return new CheckoutResponse(order.getId(), session.getUrl());
     }
 }
