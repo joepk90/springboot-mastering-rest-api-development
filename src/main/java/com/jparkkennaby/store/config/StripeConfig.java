34a9bb2e174c5082a6c2e4eddcde2335bed8820d
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-05-25 13:02:00 +0100

    08.03 - adding stripe to the project - add STRIPE_SECRET_KEY to .env.example and use the env var in the application.yaml file. create a StripeConfig class in the config package that loads the secret key

---
+++ src/main/java/com/jparkkennaby/store/config/StripeConfig.java
@@ -0,0 +1,22 @@
+package com.jparkkennaby.store.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import com.stripe.Stripe;
+
+import jakarta.annotation.PostConstruct;
+
+@Configuration
+public class StripeConfig {
+
+    @Value("${stripe.secretKey}")
+    private String secretKey;
+
+    // post construct tells springboot to call this method when this bean is created
+    // (when this class is initialised)
+    @PostConstruct
+    public void init() {
+        Stripe.apiKey = secretKey;
+    }
+}
