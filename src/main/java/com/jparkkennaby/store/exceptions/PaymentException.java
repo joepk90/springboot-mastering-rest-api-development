4b8ffe79a0fb31b29b5c7e830785b3caa929e0f1
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-05-26 13:51:06 +0100

    08.10 - refactoring the webhook - add constructor to the PaymentException exception. also add the NoArgsConstructor annotation so existing usages of the class still work

--- src/main/java/com/jparkkennaby/store/exceptions/PaymentException.java
+++ src/main/java/com/jparkkennaby/store/exceptions/PaymentException.java
@@ -1,5 +1,10 @@
 package com.jparkkennaby.store.exceptions;
 
-public class PaymentException extends RuntimeException {
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+public class PaymentException extends RuntimeException {
+    public PaymentException(String message) {
+        super(message);
+    }
 }
