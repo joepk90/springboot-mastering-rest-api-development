685732beffac64db75486ac4eb08dafaae83ae66
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-05-26 16:39:53 +0100

    09.11 - modularizing security rules - setup AuthSecurityRules rules class that implements the SecurtyRules interface - move the request matches for the auth endpoint in the SecurityConfig class to the AuthSecurityRules class

---
+++ src/main/java/com/jparkkennaby/store/security/AuthSecurityRules.java
@@ -0,0 +1,18 @@
+package com.jparkkennaby.store.security;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthSecurityRules implements SecurtyRules {
+
+    @Override
+    public void configure(
+            AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry registry) {
+        registry
+                .requestMatchers(HttpMethod.POST, "auth/login").permitAll()
+                .requestMatchers(HttpMethod.POST, "auth/refresh").permitAll();
+    }
+}
