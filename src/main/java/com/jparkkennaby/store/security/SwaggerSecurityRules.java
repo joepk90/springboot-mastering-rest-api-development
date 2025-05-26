50a9d89194f811cccf100f574ee49c637b198306
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-05-26 16:43:26 +0100

    09.11 - modularizing security rules - setup SwaggerSecurityRules rules class that implements the SecurtyRules interface - move the request matches for the swagger endpoints in the SecurityConfig class to the SwaggerSecurityRules class

---
+++ src/main/java/com/jparkkennaby/store/security/SwaggerSecurityRules.java
@@ -0,0 +1,18 @@
+package com.jparkkennaby.store.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SwaggerSecurityRules implements SecurtyRules {
+
+    @Override
+    public void configure(
+            AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry registry) {
+        registry
+                .requestMatchers("/swagger-ui/**").permitAll()
+                .requestMatchers("/swagger-ui.html").permitAll()
+                .requestMatchers("/v3/api-docs/**").permitAll();
+    }
+}
