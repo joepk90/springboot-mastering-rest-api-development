66a8b7f287cba35fc7b0736799f61f4768e048dc
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-05-26 16:36:39 +0100

    09.11 - modularizing security rules - setup ProductSecurityRules rules class that implements the SecurtyRules interface - move the request matches for the product endpoint in the SecurityConfig class to the ProductSecurityRules class

---
+++ src/main/java/com/jparkkennaby/store/security/ProductSecurityRules.java
@@ -0,0 +1,22 @@
+package com.jparkkennaby.store.security;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.stereotype.Component;
+
+import com.jparkkennaby.store.entities.Role;
+
+@Component
+public class ProductSecurityRules implements SecurtyRules {
+
+    @Override
+    public void configure(
+            AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry registry) {
+        registry
+                .requestMatchers(HttpMethod.GET, "products/**").permitAll()
+                .requestMatchers(HttpMethod.POST, "products/**").hasRole(Role.ADMIN.name())
+                .requestMatchers(HttpMethod.PUT, "products/**").hasRole(Role.ADMIN.name())
+                .requestMatchers(HttpMethod.DELETE, "products/**").hasRole(Role.ADMIN.name());
+    }
+}
