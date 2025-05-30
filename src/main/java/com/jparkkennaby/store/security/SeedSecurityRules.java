4430f8376481d748af68610edc6f2a81f9ce54ea
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-05-30 18:48:30 +0100

    add SeedSecurityRules to allow the GET method to the seed endpoint

---
+++ src/main/java/com/jparkkennaby/store/security/SeedSecurityRules.java
@@ -0,0 +1,17 @@
+package com.jparkkennaby.store.security;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SeedSecurityRules implements SecurtyRules {
+
+    @Override
+    public void configure(
+            AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry registry) {
+        registry
+                .requestMatchers(HttpMethod.GET, "/seed").permitAll();
+    }
+}
