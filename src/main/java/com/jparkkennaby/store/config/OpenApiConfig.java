5946bdaeb29b2d53679e570dbe2afe7bac9d9b1d
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-06-01 21:53:34 +0100

    setup OpenApiCustomizer logic in the OpenApiConfig class to replace the http with https for swagger requests

--- src/main/java/com/jparkkennaby/store/config/OpenApiConfig.java
+++ src/main/java/com/jparkkennaby/store/config/OpenApiConfig.java
@@ -4,11 +4,21 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+
+import org.springdoc.core.customizers.OpenApiCustomizer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.servers.Server;
+import lombok.Data;
+
 @Configuration
+@Data
 public class OpenApiConfig {
+
+    @Value("${swaggerUseSSL}")
+    private boolean swaggerUseSSL;
 
     @Bean
     public OpenAPI customOpenAPI() {
@@ -24,5 +34,23 @@ public class OpenApiConfig {
                                         .type(SecurityScheme.Type.HTTP)
                                         .scheme("bearer")
                                         .bearerFormat("JWT"))); // Optional
+    }
+
+    // hacky approach to making swagger use ssl - there is probably a more elegant
+    // solution than this
+    @Bean
+    public OpenApiCustomizer forceHttpsInServers() {
+        return openApi -> {
+            if (!swaggerUseSSL || openApi.getServers() == null) {
+                return;
+            }
+
+            for (Server server : openApi.getServers()) {
+                String url = server.getUrl();
+                if (url != null && url.startsWith("http://")) {
+                    server.setUrl(url.replace("http://", "https://"));
+                }
+            }
+        };
     }
 }
