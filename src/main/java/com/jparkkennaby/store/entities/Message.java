3ce6c682782afe9e47ebafd46ada30a660e5d132
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-06-03 18:58:04 +0100

    update Message entity to match the messages table structure

--- src/main/java/com/jparkkennaby/store/entities/Message.java
+++ src/main/java/com/jparkkennaby/store/entities/Message.java
@@ -1,10 +1,29 @@
 package com.jparkkennaby.store.entities;
 
-import lombok.AllArgsConstructor;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
+@Setter
+@Entity
+@Table(name = "messages")
 public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "text")
     private String text;
 }
