e5c1a17bac21ff172e2529326664c05a74f25785
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-06-03 18:57:27 +0100

    remove logic in the MesssageController

--- src/main/java/com/jparkkennaby/store/controllers/MesssageController.java
+++ src/main/java/com/jparkkennaby/store/controllers/MesssageController.java
@@ -1,16 +1,7 @@
 package com.jparkkennaby.store.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import com.jparkkennaby.store.entities.Message;
-
-@RestController
 public class MesssageController {
     
-    @RequestMapping("/hello")
-    public Message sayHello() {
-        return new Message("Hello World!");
-    }
 
 }
